1842d3664135343c8053a0c37b8d0d28d9a1a942
Aaron Curtis <foobarbecue@gmail.com>
2016-06-06 22:18:41 -0600

    Ask for all 10 available pages of github events

--- server/github.js
+++ server/github.js
@@ -5,17 +5,23 @@ const api_access_info = JSON.parse(Assets.getText('api_access_info.json'));
 
 const getGithub = function() {
     //Unfortunately the github API only allows querying the last 90 days or 300 events, whichever comes first
-    //TODO look into using ETag header
-    HTTP.get("https://api.github.com/users/foobarbecue/events",
-        {headers:{"User-Agent":"Meteor/1.3"}},
-        saveGithubData
-    )
+    //Ask for 10 pages at 30 items per page (30pp is not changeable)
+    for (page of [...Array(10).keys()]) {
+        //TODO look into using ETag header
+        HTTP.get("https://api.github.com/users/foobarbecue/events",
+            {
+                headers: {"User-Agent": "Meteor/1.3"},
+                params: {"page":page+1}
+            },
+            saveGithubData
+        )
+    }
 };
 
 const summaryHTML = function(item){
     switch (item.type){
         case "PushEvent":
-            return `pushed ${item.payload.commits.length} commit(s) to ${item.repo.name}`;
+            return `pushed ${item.payload.commits.length} commit(s) to <a href=${item.repo.url}>${item.repo.name}</a>`;
         case "CreateEvent":
             return `created repository <a href=${item.repo.url}>${item.repo.name}</a>`
         //TODO handle other event types e.g. issues
@@ -23,12 +29,12 @@ const summaryHTML = function(item){
 };
 
 const saveGithubData = function(error, response){
-    for (item of response.data){
-        item['service'] = 'github';
-        item['summaryHTML'] = summaryHTML(item);
-        item['timestamp'] = item.created_at;
-        Lifestreams.upsert({id:item.id},item);
-    };
+        for (item of response.data) {
+            item['service'] = 'github';
+            item['summaryHTML'] = summaryHTML(item);
+            item['timestamp'] = item.created_at;
+            Lifestreams.upsert({id: item.id}, item);
+        }
 };
 
 export default getGithub;
