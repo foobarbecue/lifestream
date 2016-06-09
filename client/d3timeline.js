84c48c9ae5b042a3fe4a9ea54757332f39bb13ca
Aaron Curtis <foobarbecue@gmail.com>
2016-06-08 23:12:06 -0600

    Add axis

--- client/d3timeline.js
+++ client/d3timeline.js
@@ -10,15 +10,16 @@ const drawLifestream = function(){
     startDate.setMonth(endDate.getMonth()-1);
     const lstrmData = Lifestreams.find().fetch();
     const tscale = d3.time.scale()
-        .range([-150, -1000])
+        .range([0, 1000])
         .domain([startDate, endDate]);
 
     d3.select('svg.lifestream')
-        .selectAll("g")
+        .selectAll("g.lane")
         .data(lstrmData)
         .enter()
         .append("g")
         .attr("transform",(d,i)=>`translate(${i*10},0)`)
+        .attr("class","lane")
       .selectAll("circle")
         .data((d, i)=>d.items) // d is lstrm[i]
         .enter()
@@ -26,6 +27,21 @@ const drawLifestream = function(){
         .attr("r",4)
         .attr("cx",10)
         .attr("cy",(d)=>tscale(d.timestamp));
+
+    let ax = d3.svg.axis()
+        .scale(tscale)
+        .orient("right")
+        .ticks(d3.time.week)
+        .tickFormat(d3.time.format("%Y-%m-%d"));
+
+    console.log('drawinaxis')
+    d3.selectAll('svg.lifestream')
+        .selectAll('g.axis')
+        .data([1])
+        .enter()
+        .append("g")
+        .attr("class","axis")
+        .call(ax)
 };
 
 Template.lifestream.helpers({
