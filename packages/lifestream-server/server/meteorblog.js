2c6b5cb955f4c182893e2385d00558565caedf36
Aaron Curtis <foobarbecue@gmail.com>
2016-07-18 18:54:43 -0600

    Only add public meteor-blog entries

--- packages/lifestream-server/server/meteorblog.js
+++ packages/lifestream-server/server/meteorblog.js
@@ -7,7 +7,7 @@ const config = Meteor.settings.lifestream.services.meteorblog;
 const getMeteorBlog = function() {
     //Unfortunately the github API only allows querying the last 90 days or 300 events, whichever comes first
     //Ask for 10 pages at 30 items per page (30pp is not changeable)
-    for (item of MeteorBlogPosts.find().fetch()){
+    for (item of MeteorBlogPosts.find({'mode':'public'}).fetch()){
         item['service'] = config.service_name;
         item['summaryHTML'] = `Created entry: <a href=/entry/${item.slug}>${item.title}</a>`;
         item['timestamp'] = new Date(item.createdAt);
